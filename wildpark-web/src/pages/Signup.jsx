4df381b6889d41d33e09ce43012a934a8b82e2dd
cxrvxxxx <brentempasisx3@gmail.com>
2023-05-29 21:48:23 +0800

    impl state for select tags in signup

--- wildpark-web/src/pages/Signup.jsx
+++ wildpark-web/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/Signup.css';
 
@@ -19,6 +19,22 @@ const PasswordMismatchBanner = () => {
 };
 
 const Signup = () => {
+    const [selectBirthdateMonth, setSelectBirthDateMonth] = useState('Month');
+    const [selectBirthdateDay, setSelectBirthdateDay] = useState('Day');
+    const [selectBirthdateYear, setSelectBirthdateYear] = useState('Year');
+
+    const handleBirthdateMonthChange = (e) => {
+        setSelectBirthDateMonth(e.target.value);
+    };
+
+    const handleBirthdateDayChange = (e) => {
+        setSelectBirthdateDay(e.target.value);
+    };
+
+    const handleBirthdateYearChange = (e) => {
+        setSelectBirthdateYear(e.target.value);
+    };
+
     return (
         <div className="row justify-content-center">
             <div className="col-md-6">
@@ -40,15 +56,15 @@ const Signup = () => {
                             <div className="form-group row d-flex justify-content-center">
                                 <label htmlFor="birthdate">Birthdate:</label>
                                 <select type="number" name="month" className="form-select col-md-3 mx-1" id="birthdateMonth"
-                                    required>
+                                    value={selectBirthdateMonth} onChange={handleBirthdateMonthChange} required>
                                     <option disabled selected value="">Month</option>
                                 </select>
                                 <select type="number" name="day" className="form-select col-md-3 mx-1" id="birthdateDay"
-                                    required>
+                                    value={selectBirthdateDay} onChange={handleBirthdateDayChange} required>
                                     <option disabled selected value="">Day</option>
                                 </select>
                                 <select type="number" name="year" className="form-select col-md-4 mx-1" id="birthdateYear"
-                                    required>
+                                    value={selectBirthdateYear} onChange={handleBirthdateYearChange} required>
                                     <option disabled selected value="">Year</option>
                                 </select>
                             </div>
